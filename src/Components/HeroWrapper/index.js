a353dc7ded0b9a555506c959df3f728be55ddb6a
Noodles <charles.pitek@gmail.com>
2020-02-28 14:01:59 -0700

    got bulma framework working

---
+++ src/Components/HeroWrapper/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+ 
+function HeroWrapper() {
+    return (
+        <section className="hero is-medium is-primary is-bold">
+            <div className="hero-body">
+                <div className="container">
+                    <h1 className="title">
+                        Primary bold title
+                    </h1>
+                    <h2 className="subtitle">
+                        Primary bold subtitle
+                    </h2>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default HeroWrapper;
