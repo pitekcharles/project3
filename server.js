8fb9230d3065721ec0c1121e101f1f9e2a895038
Charles Cressey <cmcressey@gmail.com>
2020-03-03 13:02:08 -0700

    package.json

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const path = require ("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const routes = require("./routes");
+
 
 
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +14,9 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/guildAssistant");
 
 app.get("*", (req,res) => {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
