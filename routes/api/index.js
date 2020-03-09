a46e5c6ac0ed6a902dfec2e8a8926e56c139b667
Charles Cressey <cmcressey@gmail.com>
2020-03-09 11:21:40 -0600

    heroku deployment

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const charRoutes = require("./characters");
 const guildsRoutes = require("./guilds");
+const authRoutes = require("./authRoutes");
 
 // routes
 router.use("/", authRoutes);
